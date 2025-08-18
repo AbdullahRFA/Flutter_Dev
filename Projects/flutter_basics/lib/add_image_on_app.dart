b73a2f00ab88042d4ddd738b0a4ef59af3b9f609
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-18 23:52:00 +0600

    implement how to add image on app

---
+++ Projects/flutter_basics/lib/add_image_on_app.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Image on app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Image.asset(
+              'assets/images/myself.png'
+          ),
+        ),
+      )
+    );
+  }
+}
