4f6c96c951a844e75da22762ed8a29181ae0e968
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-18 22:43:05 +0600

    implement text_Widget

---
+++ Projects/flutter_basics/lib/text_Widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title,
+        style: TextStyle(
+          fontSize: 25,
+
+        ),
+        ),
+      ),
+      body: Text("Hello World",
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.deepPurple,
+        fontWeight: FontWeight.bold,
+        backgroundColor: Colors.lightGreen,
+        height: 2,
+      ),
+      ),
+    );
+  }
+}
