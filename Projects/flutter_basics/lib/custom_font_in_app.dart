fc1e647d0ee7b4e9fdefcf335449deae809b3a22
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-24 22:44:44 +0600

    implement custom font font family

---
+++ Projects/flutter_basics/lib/custom_font_in_app.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(FlutterApp());
+}
+
+class FlutterApp extends StatelessWidget{
+  const FlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My flutter app",
+      // theme: ThemeData(
+      //   useMaterial3: false,
+      //   primarySwatch: Colors.blue
+      // ),
+
+      // If you want all AppBars in your app to have the same color:
+      theme: ThemeData(
+        useMaterial3: true, // you can keep Material 3 if you like
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white, // text & icon color
+        ),
+      ),
+      home: DashBoardScreen(),
+    );
+  }
+
+}
+
+class DashBoardScreen extends StatelessWidget{
+  const DashBoardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        // backgroundColor: Colors.blue,
+
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello World",
+            style: TextStyle(
+              fontSize: 40,
+              fontFamily: 'FontMain',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
