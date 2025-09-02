6b07df88b228391cbba77c4b4e6ff69bf554b463
AbdullahRFA <shakibrybmn@gmail.com>
2025-09-02 09:37:58 +0600

    implement date time

---
+++ Projects/flutter_basics/lib/getting current time and date.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(FlutterApp());
+}
+
+class FlutterApp extends StatelessWidget{
+  const FlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My flutter app",
+      // theme: ThemeData(
+      //   useMaterial3: false,
+      //   primarySwatch: Colors.blue
+      // ),
+
+      // If you want all AppBars in your app to have the same color:
+      theme: ThemeData(
+        useMaterial3: true, // you can keep Material 3 if you like
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white, // text & icon color
+        ),
+      ),
+      home: DashBoardScreen(),
+    );
+  }
+
+}
+class DashBoardScreen extends StatefulWidget{
+  const DashBoardScreen({super.key});
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreen();
+}
+
+class _DashBoardScreen extends State<DashBoardScreen>{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+  var time = DateTime.now();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        // backgroundColor: Colors.blue,
+
+      ),
+      body:Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Current Time $time",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.orange,
+              ),
+              ),Text("Year: ${time.year}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.green,
+              ),
+              ),Text("Month: ${time.month}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.pink,
+              ),
+              ),Text("Date:  ${time.day}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.blue,
+              ),
+              ),Text("Hour:  ${time.hour}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.black,
+              ),
+              ),Text("Minute: ${time.minute}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.greenAccent,
+              ),
+              ),Text("Current Time ${time.second}",
+              style: TextStyle(
+                fontSize: 21,
+                color: Colors.deepPurpleAccent,
+              ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+
+                    });
+                  },
+                  child: Text("Current Time")
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+}
