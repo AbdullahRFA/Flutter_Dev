323b90181ef11aa57c4783881a3a0d639db9e1b2
AbdullahRFA <shakibrybmn@gmail.com>
2025-11-26 18:53:03 +0600

    implement rich text

---
+++ Projects/flutter_basics/lib/Rich Text.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Container'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+      ),
+      body: RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 23,
+              color: Colors.grey
+            ),
+            children: <TextSpan>[
+              TextSpan(text: "Hello "),
+              TextSpan(text: "World!",
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue
+
+                )
+              ),
+              TextSpan(
+                text: " Welcome to "
+              ),
+              TextSpan(
+                text: "Flutter",
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                  fontStyle: FontStyle.italic
+
+
+              ))
+            ]
+          )
+      ),
+    );
+  }
+}
